6905790fd3ffd074096db435311535b1d1d8d6c2
Nayar Joolfoo <nayar@joolfoo.com>
2018-06-15 12:55:54 +0400

    asdd usage instruction

--- sendsms.sh
+++ sendsms.sh
@@ -1,11 +1,29 @@
-to='55555555'
-content='last one'
+#!/bin/bash
+function usage {
+    echo "Usage: sendsms.sh tonum content"
+}
+
 credentials='admin:admin'
+to=$1
+content=$2
+goip_address='192.168.100.40:84'
 
-smskey=`curl -u $credentials 'http://192.168.100.40:84/default/en_US/tools.html?type=sms' 2>/dev/null | grep smskey | sed -E 's/.*value="(.*)".*/\1/'`
+if [ -z "$to" ]
+then 
+    usage 
+    exit 1
+fi
 
-curl -u $credentials 'http://192.168.100.40:84/default/en_US/sms_info.html?type=sms' --data "line1=1&smskey=$smskey&action=SMS&telnum=$to&smscontent=$content&send=Send" > /dev/null
+if [ -z "$content" ]
+then 
+    usage 
+    exit 1
+fi
 
-curl -u $credentials 'http://192.168.100.40:84/default/en_US/send_sms_status.xml?line='
+smskey=`curl -u $credentials 'http:///default/en_US/tools.html?type=sms' 2>/dev/null | grep smskey | sed -E 's/.*value="(.*)".*/\1/'`
+
+curl -u $credentials "http://$goip_address/default/en_US/sms_info.html?type=sms" --data "line1=1&smskey=$smskey&action=SMS&telnum=$to&smscontent=$content&send=Send" > /dev/null
+
+#curl -u $credentials "http://$goip_address/default/en_US/send_sms_status.xml?line="
 
 echo $smskey
